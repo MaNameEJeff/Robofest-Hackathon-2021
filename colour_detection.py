b8d8f6219b2e68f9034bb21f7294c56dcaba91cd
Saranga Kulkarni <kulkarni.saranga@gmail.com>
2021-10-30 01:50:12 +0530

    Fixed rogue red. Replaced .jpeg with .jpg (works better due to dimensions)

--- colour_detection.py
+++ colour_detection.py
@@ -2,29 +2,31 @@ import cv2 as cv
 import numpy as np
 
 #Read the Image from path
-img = cv.imread("Test_Image.jpeg")
+path = 'D:/Saranga/Update/Robofest-Hackathon-2021/Test_Image.jpg' #Update the path variable with the path of the image
+img = cv.imread(path)
+img = cv.resize(img, (0,0), fx=2, fy=2)
 
 #Convert Image to HSV format
 img_HSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
 #HSV value range for different colours
-lower_yellow = np.array([22, 50, 0])
-upper_yellow = np.array([56, 255, 255])
+lower_yellow = np.array([22, 93, 0])
+upper_yellow = np.array([45, 255, 255])
 
-lower_green = np.array([33, 30, 30])
-upper_green = np.array([117, 255, 255])
+lower_green = np.array([50, 25, 25])
+upper_green = np.array([86, 255, 255])
 
-lower_red = np.array([0, 30, 39])
-upper_red = np.array([17, 255, 255])
+lower_red = np.array([0, 95, 175])
+upper_red = np.array([10, 255, 255])
 
-lower_blue = np.array([64, 30, 0])
-upper_blue = np.array([174, 255, 255])
+lower_blue = np.array([110, 50, 50])
+upper_blue = np.array([130, 255, 255])
 
-lower_orange = np.array([4, 29, 20])
+lower_orange = np.array([10, 100, 20])
 upper_orange = np.array([25, 255, 255])
 
-lower_white = np.array([0,0,0])
-upper_white = np.array([0,0,229])
+lower_white = np.array([0, 0, 0])
+upper_white = np.array([0, 0, 229])
 
 #Set Masks
 mask1 = cv.inRange(img_HSV, lower_yellow, upper_yellow)
@@ -56,8 +58,8 @@ def getCenter(cnt):
 	peri = cv.arcLength(cnt, True) #Gets the perimeter of the contour.
 	approx = cv.approxPolyDP(cnt, 0.02*peri, True) #Get the corner points
 
-	width = abs(approx[0][0][0] - approx[2][0][0])
-	height = abs(approx[0][0][1] - approx[2][0][1])
+	width = abs(approx[0][0][0] - approx[1][0][0])
+	height = abs(approx[0][0][1] - approx[1][0][1])
 
 	center["x"] = approx[0][0][0] + int(width/3)
 	center["y"] = approx[1][0][1] - int(height/2)
